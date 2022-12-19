021b3700fcb4de2e6028d901ce00cb4d64d9a99b
Aayush9029 <aayushpokharel36@gmail.com>
2022-12-19 14:50:30 -0500

    Initial Commit

---
+++ WatchTorrent Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchTorrent Watch App
+//
+//  Created by Aayush Pokharel on 2022-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchTorrent Watch App/WatchTorrentApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTorrentApp.swift
+//  WatchTorrent Watch App
+//
+//  Created by Aayush Pokharel on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct WatchTorrent_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
